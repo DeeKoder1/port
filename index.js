b4e4fdfea149dc01f34a2f5133126397de2eebf3
DeeKoder1 <88590677+DeeKoder1@users.noreply.github.com>
2021-12-15 12:26:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//function for displaying sidebar
+function myfunc(){
+    y = document.getElementById("sidebar");
+    y.style.display = "block";
+    y.style.width = "50%";
+}
+//function for hiding sidebar
+function func(){
+   y = document.getElementById("sidebar");
+    y.style.display = "none";
+}
+//function for hiding sidebar on blur
+function thefunc() {
+   // body = document.getElementById(the_body);
+    
+
+}
